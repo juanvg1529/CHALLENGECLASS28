0a6783e2e8596567ecb53d4fabe0ebf060a003da
juanvg1529 <67924398+juanvg1529@users.noreply.github.com>
2021-05-06 17:18:48 -0500

    1stpartdone

--- src/index.js
+++ src/index.js
@@ -1,19 +1,35 @@
-function sumar(x, y) {
+// function sumar(x, y) {
+//   console.log(x + y);
+// }
+// function restar(x, y) {
+//   console.log(x - y);
+// }
+// function multiplicar(x, y) {
+//   console.log(x * y);
+// }
+// function dividir(x, y) {
+//   console.log(x / y);
+// }
+
+// sumar(40, 3);
+// restar(14, 6);
+// multiplicar(2, 90);
+// dividir(10, 2);
+//1 challenge
+let suma = (x, y) => {
   console.log(x + y);
-}
-function restar(x, y) {
+};
+suma(2, 2);
+
+let resta = (x, y) => {
   console.log(x - y);
-}
-function multiplicar(x, y) {
+};
+resta(5, 3);
+const multiplicar = (x, y) => {
   console.log(x * y);
-}
-function dividir(x, y) {
-  console.log(x / y);
-}
-
-sumar(40, 3);
-restar(14, 6);
-multiplicar(2, 90);
-dividir(10, 2);
-
-// transforma las funciones de arriba a arrow
+};
+const divition = (x, y) => {
+  return x / y;
+};
+console.log(divition(10, 2));
+//chalenge 2
