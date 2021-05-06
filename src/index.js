52943c1aace85b0aebe6f0ff23fa29740f4a0a74
juanvg1529 <67924398+juanvg1529@users.noreply.github.com>
2021-05-06 17:04:14 -0500

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,19 @@
+function sumar(x, y) {
+  console.log(x + y);
+}
+function restar(x, y) {
+  console.log(x - y);
+}
+function multiplicar(x, y) {
+  console.log(x * y);
+}
+function dividir(x, y) {
+  console.log(x / y);
+}
+
+sumar(40, 3);
+restar(14, 6);
+multiplicar(2, 90);
+dividir(10, 2);
+
+// transforma las funciones de arriba a arrow
